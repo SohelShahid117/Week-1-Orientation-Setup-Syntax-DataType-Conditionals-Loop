52e91b913013f625de14acf4c59dcae968d11d90
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-05 16:31:22 +0600

    c programming setup & hello world part done

---
+++ Module-0-Orientation & Setup/basic.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("all is wellll\n");
+    printf("hello world\n");
+    return 0;
+}
+// ctrl+shift+b
---
+++ Module-0-Orientation & Setup/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a;
+    printf("value of a : ");
+    scanf("%d",&a);
+    printf("a=%d\n",a);
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello 2\n");
+    printf("%d\n",5);
+    int x = 5*2;
+    printf("%d\n",x);
+    return 0;
+}
