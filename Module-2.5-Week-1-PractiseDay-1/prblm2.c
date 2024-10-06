5723e5a5c097ed732df77cdd7c7675a3a061127c
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-06 17:53:57 +0600

    done 2.5.2 Practice Instruction hacker rank

---
+++ Module-2.5-Week-1-PractiseDay-1/prblm2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    if (N == 0)
+    {
+        printf("Zero");
+    }
+    else
+    {
+        printf("Non Zero");
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
+
+/*
+//Zero or Non Zero
+
+in this problem you will be given an integer number N. Print "Zero" if the number is equals to 0 and "Non Zero" Otherwise.
+
+Input Format
+
+    The input consists of an integer N.
+
+Constraints
+
+-1000 <= N <= 1000
+
+Output Format
+
+    Print "Zero" if the number is equals to 0 and "Non Zero" Otherwise.
+
+Sample Input
+5
+
+Sample Output
+Non Zero
+*/
---
+++ Module-2.5-Week-1-PractiseDay-1/problem1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    //Programming Begins
+
+    In this problem you will just have to print the line "Hello world!" (without quotation)
+
+    Sample Output:
+    Hello World!
+    */
+
+    printf("Hello World!");
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
