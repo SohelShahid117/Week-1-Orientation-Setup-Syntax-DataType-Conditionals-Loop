aacc9fd68f9a096fa0e996954ff544660b027f9f
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-09 21:32:51 +0600

    Conceptual Session two [Conditional Statements and Loop]--20min done

---
+++ week-1-conceptual-session/controlStatment.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("\nthere are 3 types of Control statement\n");
+    printf(" 1.Conditional statement(if-else,switch-case)\n 2.Iterative statement(loop)\n 3.Jump Statement(goto,continue,break)\n");
+
+    //Conceptual Session two [Conditional Statements and Loop]--20min done
+
+    return 0;
+}
